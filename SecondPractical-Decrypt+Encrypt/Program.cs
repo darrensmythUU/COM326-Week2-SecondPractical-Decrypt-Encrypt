1b28df9388609ff6b263bd90c63d214b4346bbf3
Kiphea <djsmyth05@gmail.com>
2025-10-06 10:08:20 +0100

    Task 5 Complete + Setup Complete

--- SecondPractical-Decrypt+Encrypt/Program.cs
+++ SecondPractical-Decrypt+Encrypt/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Practical 2
+ * Information: Methods Demo (Decrypt+Encrypt)
+ * Version 1
+ * Author: Darren Smyth
+ * Date: October 6th 2026
+ */
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        CountWords();
+    }
+
+    public static void CountWords()
+    {
+        Console.WriteLine("Please enter a sentence while capitalizing each first letter of each word.");
+        string userSentence = Console.ReadLine();
+
+        // Iterates throughout the char array version of userSentence to figure out how many blank spaces there are to figure out how many words there are
+        char[] splitUserSentence = userSentence.ToCharArray();
+        int numberOfWords = 0;
+        for (int i = 0; i < splitUserSentence.Length; i++)
+        {
+            if (splitUserSentence[i] == ' ')
+            {
+                numberOfWords++;
+            }
+        }
+
+        Console.WriteLine($"The sentence you inputted is: {userSentence}");
+        Console.WriteLine($"Number of words: {numberOfWords}.");
+    }
+}
