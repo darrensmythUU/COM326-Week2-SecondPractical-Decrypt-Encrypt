68dac0fc93a1293e24d5d1b68a50f4de3b08405d
Kiphea <djsmyth05@gmail.com>
2025-10-06 10:16:18 +0100

    Task 5 Remade

--- SecondPractical-Decrypt+Encrypt/Program.cs
+++ SecondPractical-Decrypt+Encrypt/Program.cs
@@ -16,17 +16,8 @@ class Program
     {
         Console.WriteLine("Please enter a sentence while capitalizing each first letter of each word.");
         string userSentence = Console.ReadLine();
-
-        // Iterates throughout the char array version of userSentence to figure out how many blank spaces there are to figure out how many words there are
-        char[] splitUserSentence = userSentence.ToCharArray();
-        int numberOfWords = 0;
-        for (int i = 0; i < splitUserSentence.Length; i++)
-        {
-            if (splitUserSentence[i] == ' ')
-            {
-                numberOfWords++;
-            }
-        }
+        string[] splitUserSentence = userSentence.Split(' ');
+        int numberOfWords = splitUserSentence.Length;
 
         Console.WriteLine($"The sentence you inputted is: {userSentence}");
         Console.WriteLine($"Number of words: {numberOfWords}.");
