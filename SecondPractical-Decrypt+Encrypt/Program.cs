e84ba77a23cb33763d67d6b95b734e4df8fc2cdf
Kiphea <djsmyth05@gmail.com>
2025-11-08 14:22:49 +0000

    Task 9 Complete + General Improvements/Bug Fixes

--- SecondPractical-Decrypt+Encrypt/Program.cs
+++ SecondPractical-Decrypt+Encrypt/Program.cs
@@ -14,28 +14,27 @@ class Program
     public static void Main(string[] args)
     {
         //CountWords();
-        int option = 0;
+        int option = 5; // 5 is chosen instead of 0 to stop environment from auto-exiting if user inputs invalid value
         bool exitBoolean = false;
         do
         {
-            option = 0;
             PrintEncryptDecryptMenu();
             option = InputOption(option);
 
             if (option == 0)
             {
-                exitBoolean = true;
+                Environment.Exit(0);
             }
             else if (option == 1)
             {
                 Encrypt();
             }
-            else
+            else if (option == 2)
             {
                 Decrypt();
             }
+            option = 5;
         } while (!exitBoolean);
-        Environment.Exit(0);
     }
 
     private static void PrintEncryptDecryptMenu()
@@ -43,7 +42,7 @@ class Program
         // Prints a menu which allows the user to see the below options
         Console.WriteLine("----------------------------------------");
         Console.WriteLine("Main Menu");
-        Console.WriteLine("Select an option from below: ");
+        Console.WriteLine("Please select an option from below: ");
         Console.WriteLine("1 - Encrypt Text");
         Console.WriteLine("2 - Decrypt Text");
         Console.WriteLine("0 - End");
@@ -59,7 +58,7 @@ class Program
         catch (FormatException ex)
         {
             // Handles the case where the input is not valid
-            Console.WriteLine("Please enter a valid number.");
+            Console.WriteLine("Please enter a valid number (0 - 2).");
         }
         return methodOption;
     }
@@ -78,20 +77,31 @@ class Program
 
     public static void Encrypt()
     {
-        Console.WriteLine("Enter a string that you wish to encrypt.");
+        Console.WriteLine("Enter the desired string that you wish to encrypt.");
         string userSentence = Console.ReadLine();
         char[] splitUserSentence = userSentence.ToCharArray();
 
-        Console.WriteLine("Enter the number of rotations you wish to encrypt by");
-        int rotationAmount = Convert.ToInt32(Console.ReadLine());
+        int rotationAmount = NumberOfRotationsAcquirer(0) % 25;
+        // Done so that rotationAmount can be reset at the start of each for loop at line 95
 
         // ASCII alphabet used to simplify char rotations
         byte[] asciiBytes = Encoding.ASCII.GetBytes(userSentence);
+        bool isUpper = true;
         for (int i = 0; i < splitUserSentence.Length; i++)
         {
-            // If ASCII code is not a SPACE, rotates forwards by 2. Done to stop all spaces becoming speechmarks
-            if (asciiBytes[i] != 32)
+            // Only rotates forwards if the given ASCII value is a letter. Done to allow only letters to be encrypted
+            if ((asciiBytes[i] >= 65 && asciiBytes[i] <= 90) || (asciiBytes[i] >= 97 && asciiBytes[i] <= 122))
             {
+                isUpper = char.IsUpper(splitUserSentence[i]);
+                // Checks if rotation amount would cause asciiBytes to loop over the alphabet
+                if ((asciiBytes[i] + rotationAmount) > 90 && isUpper == true)
+                {
+                    asciiBytes[i] = 64;
+                }
+                else if ((asciiBytes[i] + rotationAmount) > 122 && isUpper == false)
+                {
+                    asciiBytes[i] = 96;
+                }
                 asciiBytes[i] += (byte)rotationAmount;
                 splitUserSentence[i] = (char)asciiBytes[i];
             }
@@ -104,20 +114,30 @@ class Program
 
     public static void Decrypt()
     {
-        Console.WriteLine("Enter a string that you wish to decrypt.");
+        Console.WriteLine("Enter the desired string that you wish to decrypt.");
         string userSentence = Console.ReadLine();
         char[] splitUserSentence = userSentence.ToCharArray();
 
-        Console.WriteLine("Enter the number of rotations you wish to decrypt by");
-        int rotationAmount = Convert.ToInt32(Console.ReadLine());
+        int rotationAmount = NumberOfRotationsAcquirer(0) % 25;
 
         // ASCII alphabet used to simplify char rotations
         byte[] asciiBytes = Encoding.ASCII.GetBytes(userSentence);
+        bool isUpper = true;
         for (int i = 0; i < splitUserSentence.Length; i++)
         {
-            // If ASCII code is not a SPACE, rotates backwards by 2. Done to stop all spaces becoming speechmarks
-            if (asciiBytes[i] != 32)
+            // Only rotates backwards if the given ASCII value is a letter. Done to allow only letters to be encrypted
+            if ((asciiBytes[i] >= 65 && asciiBytes[i] <= 90) || (asciiBytes[i] >= 97 && asciiBytes[i] <= 122))
             {
+                isUpper = char.IsUpper(splitUserSentence[i]);
+                // Checks if rotation amount would cause asciiBytes to loop backwards over the alphabet
+                if ((asciiBytes[i] - rotationAmount) < 65 && isUpper == true)
+                {
+                    asciiBytes[i] = 91;
+                }
+                else if ((asciiBytes[i] - rotationAmount) < 97 && isUpper == false)
+                {
+                    asciiBytes[i] = 123;
+                }
                 asciiBytes[i] -= (byte)rotationAmount;
                 splitUserSentence[i] = (char)asciiBytes[i];
             }
@@ -126,5 +146,35 @@ class Program
 
         Console.WriteLine($"The sentence you inputted is: {userSentence}");
         Console.WriteLine($"The decrypted sentence is now: {decryptedUserSentence}.");
+    }
+
+    public static int NumberOfRotationsAcquirer (int methodRotationAmount)
+    {
+        bool exitBoolean = false;
+        do
+        {
+            try
+            {
+                methodRotationAmount = 0;
+                Console.Write("Enter the desired number of rotations (0-100): ");
+                methodRotationAmount = Convert.ToInt32(Console.ReadLine());
+
+                if (methodRotationAmount < 0 || methodRotationAmount > 100)
+                {
+                    Console.WriteLine("You cannot enter a number less than 0 times or greater than 100. Please enter a valid number.");
+                }
+                else
+                {
+                    exitBoolean = true;
+                }
+            }
+            catch (FormatException ex)
+            {
+                // Handles the case where the input is not valid
+                Console.WriteLine("Please enter a valid number.");
+            }
+        } while (exitBoolean == false);
+
+        return methodRotationAmount;
     }
 }
